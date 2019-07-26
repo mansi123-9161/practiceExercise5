8f7f139a3a262001f66d384f01b42a5d552c398d
mansi <mansi.chourasia@cgi.com>
2019-07-26 08:40:39 +0530

    updated comment for logic of countFrequency

--- src/main/java/com/stackroute/pe5/CountFrequency.java
+++ src/main/java/com/stackroute/pe5/CountFrequency.java
@@ -1,6 +1,10 @@
 package com.stackroute.pe5;
 import java.util.*;
 public class CountFrequency {
+    /*Write a program where an array of strings is input and output is a Map<String,boolean> where
+each different string is a key and its value is true if that string appears 2 or more times in the array
+Input : String arr[] = {“a”,”b”,”c”,”d”,”a”,”c”,”c”}
+Output - {“a” : true,”b” :false ,”c” :true,”d” : false}*/
     public String checkFrequencyOfCharector(String stringData){
         if(stringData==null)
 
@@ -15,7 +19,7 @@ public class CountFrequency {
                 map.put(string, 1);
             }
         }
-
+// for printing true if key appears more than one time
         Map<String,Boolean> mapValue=new HashMap<>();
         for(String string:map.keySet()){
             if(map.get(string)>=2){
